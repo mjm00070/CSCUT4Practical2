ac1f8ce083b9f73d5753c13dcc77fbfc4861c0aa
mjm00070 <mjm00070@students.stir.ac.uk>
2021-02-18 16:42:31 +0000

    First commit

--- FilesInOut.java
+++ FilesInOut.java
@@ -4,31 +4,55 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 import java.lang.Number;
+import java.lang.Object;
 
 /**
  * 
  * CSCU9T4 Java strings and files exercise.
  *
  */
-public class FilesInOut {
+public class FilesInOut
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+    	File inputFile = new File("input.txt");
+    	Scanner in = new Scanner(inputFile);
+    	PrintWriter out = new PrintWriter("output.txt");
+    	String tempToken;
+    	ArrayList allNames = new ArrayList();
 
-    public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+    	while(in.hasNext())
+    	{
+    		tempToken = in.next();
+    		if(!isNumeric(tempToken))
+    		{
+    			allNames.add(tempToken);
+    			//out.println(tempToken.toUpperCase());
 
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
-
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
-
+    		}
+    	}
+    	in.close();
+    	out.close();
         // Finally, add code to read the filenames as arguments from the command line.
 
-        System.out.println("You need to add your own code to do anything");
-
+        System.out.println("hello mate");
     } // main
+
+    public static boolean isNumeric(String strNum)
+    {
+        if (strNum == null)
+        {
+            return false;
+        }
+        try
+        {
+            double d = Double.parseDouble(strNum);
+        }
+        catch (NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
+    }
 
 } // FilesInOut
