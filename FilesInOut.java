cea3c3314450ec24569171e5fcef27103b65fe0a
mjm00070 <mjm00070@students.stir.ac.uk>
2021-02-19 13:49:37 +0000

    Assigning names to 2d array and numbers to array completed for part 2

--- FilesInOut.java
+++ FilesInOut.java
@@ -13,22 +13,15 @@ import java.lang.Object;
  */
 public class FilesInOut
 {
-    public static void main(String[] args) throws FileNotFoundException
+    /*public static void main(String[] args) throws FileNotFoundException
     {
-    	/*File inputFile = new File("input.txt");
-    	Scanner in = new Scanner(inputFile);
-    	PrintWriter out = new PrintWriter("output.txt");
-    	String tempToken;
-    	ArrayList allNames = new ArrayList();
-    	ArrayList allNumbers = new ArrayList();
-    	boolean isUppercase = false;*/
     	boolean isUppercase;
     	File inputFile;
     	Scanner in;
     	PrintWriter out;
     	String tempToken;
-    	ArrayList allNames = new ArrayList();
-    	ArrayList allNumbers = new ArrayList();
+    	ArrayList<String> allNames = new ArrayList<String>();
+    	ArrayList<String> allNumbers = new ArrayList<String>();
     	if(args[0].toUpperCase().equals("-u".toUpperCase())) //if "-u" is given as a parameter
     	{
     		isUppercase = true;
@@ -75,7 +68,88 @@ public class FilesInOut
     	in.close();
     	out.close();
 
-        System.out.println("hello mate");
+        System.out.println("Program has been executed");
+    } // main*/
+	
+    public static void main(String[] args) throws FileNotFoundException
+    {
+    	//JUST FOR TESTING
+    	String[] testArgs = new String[3];
+    	testArgs[0] = "inputm.txt";
+    	testArgs[1] = "output.txt";
+    	
+    	boolean isUppercase;
+    	File inputFile;
+    	Scanner in;
+    	PrintWriter out;
+    	String tempToken;
+    	ArrayList<String> allNames = new ArrayList<String>();
+    	ArrayList<String> allNumbers = new ArrayList<String>();
+    	ArrayList<ArrayList<String>> groupedNames = new ArrayList<ArrayList<String>>();
+    	if(testArgs[0].toUpperCase().equals("-u".toUpperCase())) //if "-u" is given as a parameter
+    	{
+    		isUppercase = true;
+    		inputFile = new File(testArgs[1]);
+    		out = new PrintWriter(testArgs[2]);
+    	}
+    	else
+    	{
+    		isUppercase = false;
+        	inputFile = new File(testArgs[0]);
+        	in = new Scanner(inputFile);
+        	out = new PrintWriter(testArgs[1]);
+    	}
+    	in = new Scanner(inputFile);
+    	
+    	int lineCounter = 0;
+    	while(in.hasNextLine())
+    	{
+    		tempToken = in.next();
+    		groupedNames.add(new ArrayList<String>()); //A new arraylist is added to groupedNames for each line in the input file
+    		while(!isNumeric(tempToken)) //Checks every name on the current line
+    		{
+    			groupedNames.get(lineCounter).add(tempToken); //Adds the name to the arraylist
+    			tempToken = in.next();
+    		}
+    		if(isNumeric(tempToken))
+    		{
+    			allNumbers.add(tempToken); //Adds the phone number to allNumbers
+    		}
+    		lineCounter++;
+    	}
+    	
+    	/*while(in.hasNext())
+    	{
+    		tempToken = in.next();
+    		if(!isNumeric(tempToken))
+    		{
+    			allNames.add(tempToken);
+    		}
+    		else
+    		{
+    			allNumbers.add(tempToken);
+    		}
+    	}*/
+    	
+    	int numberCounter = 0;
+    	
+    	for(int i = 0; i < allNames.size(); i += 2)
+    	{
+    		if(isUppercase)
+    		{
+        		out.print(allNames.get(i).toString().toUpperCase() + " " + allNames.get(i+1).toString().toUpperCase() + " ");
+    		}
+    		else
+    		{
+    			out.print(convertToTitleCaseIteratingChars(allNames.get(i).toString() + " " + allNames.get(i + 1)) + " ");
+    		}
+    		out.println(allNumbers.get(numberCounter));
+    		numberCounter++;
+    	}
+    	in.close();
+    	out.close();
+
+        System.out.println("Program has been executed");
     } // main
 
     public static boolean isNumeric(String strNum)
